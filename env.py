38a6783dcfd03ab48dabf012aafa2cf181fad4c4
Neel Kant <kantneel@gmail.com>
2020-02-11 17:16:11 -0800

    Write EnvState and refactor CBNEnv.create()

--- env.py
+++ env.py
@@ -1,13 +1,21 @@
-from stable_baselines.common.vec_env import DummyVecEnv
 from gym import Env
+from gym.spaces import Discrete, Box
+import numpy as np
+from stable_baselines.common.vec_env import DummyVecEnv
+
+from .causal import CausalGraph
 
 
-class CBNVecEnv(Env):
+class CBNEnv(Env):
     def __init__(self):
         """Create a stable_baselines-compatible environment to train policies on"""
-        # set action_space
-        # set observation_space
-        pass
+        self.action_space = Discrete(8)
+        self.observation_space = Box(-5, 5, (8,))
+        self.state = EnvState()
+
+    @classmethod
+    def create(cls, n_env):
+        return DummyVecEnv([lambda: cls for _ in range(n_env)])
 
     def step(self, action):
         pass
@@ -25,5 +33,11 @@ class CBNVecEnv(Env):
         pass
 
 
-def create_env(n_env):
-    return DummyVecEnv([lambda: CBNVecEnv for _ in range(n_env)])
+class EnvState(object):
+    def __init__(self):
+        """Create an object which holds the state of a CBNEnv"""
+        self.info_phase = True
+        self.graph = CausalGraph()
+
+    def intervene(self, node_idx, val):
+        self.graph.intervene(node_idx, val)
